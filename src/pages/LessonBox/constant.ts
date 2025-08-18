25d8b713e7b857b238c87456c7961cd588a5740d
Pixel-Daze <zhixin.zhu@ximalaya.com>
2025-08-19 01:05:40 +0800

    feat: 减少图片资源

--- src/pages/LessonBox/constant.ts
+++ src/pages/LessonBox/constant.ts
@@ -1,25 +1,22 @@
 import { type BoxCellInfo } from "./type";
-import PIC_BOX_1 from '@/assets/imgs/pic_box_1.jpg';
-import PIC_BOX_2 from '@/assets/imgs/pic_box_2.jpg';
-import PIC_BOX_3 from '@/assets/imgs/pic_box_3.jpg';
 
 export const DATA_LIST: BoxCellInfo[] = [
     {
-        cover: PIC_BOX_1,
+        cover: 'https://p0.pipi.cn/mediaplus/friday_image_fe/0fa334b7801e325294a219a9c28f60099c482.jpg?imageMogr2/thumbnail/2500x2500%3E',
         id: 1,
         title: '浪浪山小妖怪',
         desc: '暑期必看合家欢！超高分IP回归！浪浪山，和蛤蟆精、黄鼠狼精、猩猩怪组团取经。西行路上，小妖怪们将遇到怎样的考验？最终他们又能否实现心愿，活成自己喜欢的样子？',
         linkUrl: 'https://www.maoyan.com/films/1515448',
     },
     {
-        cover: PIC_BOX_2,
+        cover: 'https://p0.pipi.cn/mediaplus/friday_image_fe/0fa334bf05177ed818160df07ead6c0ce17aa.jpg?imageMogr2/thumbnail/2500x2500%3E',
         id: 2,
         title: '捕风追影',
         desc: '高能预警，年度狠戏即将开场！成龙化身心眼子玩家，和张子枫组成师徒极限追凶；梁家辉爆改疯批Daddy，带领「狼系养子团」上演癫狂厮杀！ 一伙天才盗匪劫走数亿资产，却凭借超强反侦察能力全身而退，戏耍警方“天眼”系统。一筹莫展之际，澳门司警局请回了隐退多年的跟踪专家黄德忠（成龙 饰），他培养年轻司警何秋果（张子枫 饰）等人，重组“神秘跟踪队”，最终锁定了盗匪团的幕后狼王傅隆生（梁家辉 饰）。当警方布下天罗地网之时，盗匪团也设下局中局，斗智斗勇斗心眼，一场高端猫鼠局拉开帷幕......',
         linkUrl: 'https://www.maoyan.com/films/1505571',
     },
     {
-        cover: PIC_BOX_3,
+        cover: 'https://p0.pipi.cn/mediaplus/friday_image_fe/0fa334bf4ea8d3a21915d50407599cb349dfd.jpg?imageMogr2/thumbnail/2500x2500%3E',
         id: 3,
         title: '罗小黑战记2',
         desc: '暑期口碑王炸，二维原创国产动画标杆！阔别6年，高人气治愈动画电影续作全面升级归来！萌力全开暑假必看！快来跟随罗小黑与无限，开启全新奇幻冒险吧！ 小黑跟随师父在小镇上过着安宁日子，分会馆遇袭，打破了妖精世界维系多年的和平，来自妖灵会馆的“全新任务”也让两人陷入分离危机，更让师父无限的处境发生翻天覆地的变化。面对环环相扣的谜团，小黑与师姐鹿野共同开启了寻找真相的新冒险。这一次小黑能否经受住考验，顺利保护师父化险为夷？',

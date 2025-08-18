2bc2f43c639b8b489af613f9eecd03ec4957fca2
Pixel-Daze <zhixin.zhu@ximalaya.com>
2025-08-19 00:43:42 +0800

    feat: 第二课代码补充

--- src/pages/LessonBox/index.tsx
+++ src/pages/LessonBox/index.tsx
@@ -7,9 +7,12 @@ const LessonBox = () => {
   return (
     <div>
         <div className={styles.lessonBoxContainer}>
-            {DATA_LIST && DATA_LIST.length > 0 ? DATA_LIST.map((item: BoxCellInfo, index: number) => {
+            {/* {DATA_LIST && DATA_LIST.length > 0 ? DATA_LIST.map((item: BoxCellInfo, index: number) => {
                 return <BoxCell key={index} info={item} />
-            }) : null}
+            }) : null} */}
+            <BoxCell  info={DATA_LIST[0]} />
+            <BoxCell  info={DATA_LIST[1]} />
+            <BoxCell  info={DATA_LIST[2]} />
         </div>
         {/* <div className={styles.lessonBoxContainer}>
             {DATA_LIST && DATA_LIST.length > 0 ? DATA_LIST.map((item: BoxCellInfo, index: number) => {
